2d027df4e354e8c2b3bc7bac000676fe0c56eac7
Geoffrey Fielden-Briggs <fielden.geoffrey@gmail.com>
2016-08-24 23:39:58 -0400

    First commit

---
+++ app/src/main/java/org/fieldenbriggs/petshop/ActivitylistItems.java
@@ -0,0 +1,13 @@
+package org.fieldenbriggs.petshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitylistItems extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activitylist_items);
+    }
+}
