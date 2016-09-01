e9099526a6a40fffd075afac35ad65fc139e0451
Fielden-Briggs Geoffrey <1354177@cegepmontpetit.ca>
2016-09-01 14:46:00 -0400

    Main activiy login

--- app/src/main/java/org/fieldenbriggs/petshop/Interface/IService.java
+++ app/src/main/java/org/fieldenbriggs/petshop/Interface/IService.java
@@ -1,7 +1,21 @@
 package org.fieldenbriggs.petshop.Interface;
 
+import org.fieldenbriggs.petshop.model.Animalerie;
+import org.fieldenbriggs.petshop.model.Utilisateur;
+
 /**
  * Created by Geoffrey on 8/31/2016.
  */
 public interface IService {
+
+
+    /**
+     * Méthode qui permet de vérifier si un utilisateur est valide pour pouvoir entrer dans l'application
+     * @return Si l'utilisateur est valide ou non.
+     */
+    public Boolean verifierUtilisateur(String pCourriel , String pMotDePasse , Animalerie pAnimalerie);
+
+
+
+
 }
--- app/src/main/java/org/fieldenbriggs/petshop/LoginActivity.java
+++ app/src/main/java/org/fieldenbriggs/petshop/LoginActivity.java
@@ -1,7 +1,13 @@
 package org.fieldenbriggs.petshop;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Identification des boutons
+        Button btnLogin = (Button) findViewById(R.id.btnLog);
+        Button btnCompte = (Button) findViewById(R.id.btnNew);
+        // Identification des textfields
+       // EditText txtUtil = (EditText) findViewById(R.id.)
+        // Listener de connexion
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //// TODO: 2016-09-01 Faire eventuellement une vérification d'utilisateur
+                Intent intentLoging = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intentLoging);
+            }
+        });
+
+        // Listener de creation de compte
+        btnCompte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Pas encore implementé!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
